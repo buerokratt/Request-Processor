eed56ba3d677c831be2de76e9cd1fed9eb486fb9
joonasroosalung <104758060+joonasroosalung@users.noreply.github.com>
2022-06-14 16:38:20 +0300

    Extract DMR service to new project (#6)
    * feat: #3 Create base for new project
    
    * feat: #3 Create base for new project
    
    * feat: #3 Create tests
    
    * feat: #3 Create tests and bring new logging from dmr
    
    * feat: #3 Fix up tests and test project
    
    * chore: #3 Create github workflows
    
    * chore: #3 Disable nullable in RequestProcessor project
    
    * chore: #3 Exclude code from coverage
    
    * chore: #3 Exclude code from coverage
    
    * chore: #3 Exclude even service from code coverage to cheat github actions
    
    * chore: #3 Add netanalyzers package
    
    * chore: #3 Add netanalyzers package
    
    * chore: #3 Add netanalyzers package
    
    * chore: #3 Remove netanalyzers package, attempt to add stryker correctly
    
    * chore: #3 netanalyzers again
    
    * chore: #3 Properly configure dotnet tools
    
    * chore: #3 Squeak through
    
    * chore: #3 Remove unnecessary docker stuff
    
    * chore: #3 Same old

---
+++ src/RequestProcessor/Models/HeadersInput.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RequestProcessor.Models
+{
+    // No logic so no unit tests are required
+    [ExcludeFromCodeCoverage]
+    public class HeadersInput
+    {
+        [Required]
+        [FromHeader(Name = Constants.XSentByHeaderName)]
+        public string XSentBy { get; set; }
+
+        [Required]
+        [FromHeader(Name = Constants.XSendToHeaderName)]
+        public string XSendTo { get; set; }
+
+        [Required]
+        [FromHeader(Name = Constants.XMessageIdHeaderName)]
+        public string XMessageId { get; set; }
+
+        [FromHeader(Name = Constants.XMessageIdRefHeaderName)]
+        public string XMessageIdRef { get; set; }
+
+        [FromHeader(Name = Constants.XModelTypeHeaderName)]
+        public string XModelType { get; set; }
+
+        [FromHeader(Name = Constants.ContentTypeHeaderName)]
+        public string ContentType { get; set; }
+    }
+}
---
+++ src/RequestProcessor/Models/Message.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace RequestProcessor.Models
+{
+    // No logic so no unit tests are required
+    [ExcludeFromCodeCoverage]
+    public class Message
+    {
+        public string Payload { get; set; }
+        public HeadersInput Headers { get; set; }
+    }
+}
