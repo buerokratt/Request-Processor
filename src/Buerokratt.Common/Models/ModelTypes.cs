232bb4fa2b091825f5b622612625d2db39fd432e
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-20 16:50:05 +0100

    Remove mockbot specific model type

--- src/Buerokratt.Common/Models/ModelTypes.cs
+++ src/Buerokratt.Common/Models/ModelTypes.cs
@@ -16,11 +16,5 @@
         /// Content type for routing a message via the DMR.
         /// </summary>
         public static readonly string MessageRequest = "application/vnd.dmr.messagerequest+json;version=1";
-
-        /// <summary>
-        /// Content type for indicating acknowledgement of receiving a message. This content type is to indicate that the message in the payload is not one that requires any kind of reply.
-        /// This is a type that is used by Mock Bots only. 
-        /// </summary>
-        public static readonly string MessageAcknowledgement = "application/vnd.mockbot.messageacknowledge+json;version=1";
     }
 }
